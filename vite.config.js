c00514b8e359a7b2dea6e1b435eecc320ced8506
Evan <diosaaqua69@gmail.com>
2026-07-03 14:33:12 -0600

    Config for automated Deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/valve-designer-app/', // ← Agregamos esto con el nombre exacto de tu repositorio
 })
